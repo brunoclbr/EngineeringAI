23c63b550b6e4aee6e1c8618c75478abc8b2ee57
brunoclbr <bruno.copa@rwth-aachen.de>
2024-05-24 11:29:59 +0100

    started conv model

--- keras_deep_dive/convnets.py
+++ keras_deep_dive/convnets.py
@@ -21,4 +21,67 @@ Create new branch to practice Git???
 And practice coding with the book and collab?
 """
 
-import numpy as np
+import os, shutil, pathlib
+import conv_model1
+from tensorflow import keras
+from keras.utils import image_dataset_from_directory # set up data pipeline
+
+original_dir = pathlib.Path("train")
+new_base_dir = pathlib.Path("cats_vs_dogs_small")
+
+def make_subset(subset_name, start_index, end_index):
+    for category in ("cat", "dog"):
+        dir = new_base_dir / subset_name / category 
+        os.makedirs(dir)
+        fnames = [f"{category}.{i}.jpg" 
+                  for i in range(start_index, end_index)]
+        for fname in fnames:
+            shutil.copyfile(src=original_dir / fname, dst=dir / fname)
+
+make_subset("train", start_index=0, end_index=1000)
+make_subset("validation", start_index=1000, end_index=1500)
+make_subset("test", start_index=1500, end_index=2500)
+
+model = conv_model1
+
+# transform images into preprocessed floating-point tensors with a generator
+# 1) read files 2) JPEG --> RGB grid of pixels 3) convert into floating points 4) resize 5) pack into batches
+
+train_dataset = image_dataset_from_directory(
+    new_base_dir / "train",
+    image_size= (180, 180),
+    batch_size=32
+)
+
+validation_dataset = image_dataset_from_directory(
+    new_base_dir / "validation",
+    image_size= (180, 180),
+    batch_size=32
+)
+
+test_dataset = image_dataset_from_directory(
+    new_base_dir / "test",
+    image_size= (180, 180),
+    batch_size=32
+)
+
+# quick look into one Dataset object
+for data_batch, labels_batch in train_dataset:
+    print("data batch shape:", data_batch.shape)
+    print("labels batch shape:", labels_batch.shape)
+    break
+
+callbacks = [
+    keras.callbacks.ModelCheckpoint(
+        filepath="convnet_from_scratch.keras",
+        save_best_only=True,
+        monitor="val_loss"
+    )
+]
+
+history = model.fit(
+    train_dataset,
+    epochs=30,
+    validation_data=validation_dataset,
+    callbacks=callbacks
+)
